3566e0e833fdc982b1fbc3cbc71d3c3491079591
juanjuanzero-dev <juanjuanzero.dev@gmail.com>
2023-07-09 14:54:51 -0400

    actually just use the ananke theme

--- themes/juanTrickPony/static/js/bs-tabs.js
+++
@@ -1,9 +0,0 @@
-const triggerTabList = document.querySelectorAll('#nav-link a')
-triggerTabList.forEach(triggerEl => {
-  const tabTrigger = new bootstrap.Tab(triggerEl)
-
-  triggerEl.addEventListener('click', event => {
-    event.preventDefault()
-    tabTrigger.show()
-  })
-})
